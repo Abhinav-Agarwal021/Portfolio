330f1f2c05a0be74aa2811471cc313ed376ba744
Abhinav Agarwal <agarwalabhinav309@gmail.com>
2022-03-20 18:45:03 +0530

    portfolio updated

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,65 +11,92 @@ import eight from '../assets/svg/projects/eight.svg'
 export const projectsData = [
     {
         id: 1,
+        projectName: 'Virtual Meet',
+        projectDesc: 'Project made for group chat and video chat and for dm. we can manage people under some particular roles. I am still working on this project to increase some more features like sharing white-board etc.',
+        tags: ['React', 'CSS', "Express", "Mongodb"],
+        code: 'https://github.com/Abhinav-Agarwal021/Virtual-Meet',
+        demo: '',
+        image: one
+    },
+    {
+        id: 2,
+        projectName: 'Any Share',
+        projectDesc: 'Project made to share any large file remotely and locally. I am still working on project on sharing files locally.',
+        tags: ['Flutter', "Django", "PostgreSql"],
+        code: 'https://github.com/Abhinav-Agarwal021/any_share',
+        demo: '',
+        image: two
+    },
+    {
+        id: 3,
+        projectName: 'Open Source Career Aid',
+        projectDesc: 'Project made to make all resources available for free for any course rather it is core or any technical skill.',
+        tags: ['React', 'CSS', "Django"],
+        code: '',
+        demo: 'https://osca-production.herokuapp.com/',
+        image: three
+    },
+    {
+        id: 4,
         projectName: 'Interactive Audio-video quiz',
         projectDesc: 'This project was made for screen size 3840*2160 for a museum',
         tags: ['React', 'CSS'],
         code: '',
         demo: 'https://main.d1j6x84k1680pw.amplifyapp.com/',
-        image: one
+        image: four
     },
     {
-        id: 2,
+        id: 5,
         projectName: 'Memory Game',
         projectDesc: 'This project was made for screen size 3840*2160 for a museum',
         tags: ['React', 'CSS'],
         code: '',
         demo: 'https://game.d1cz8cp3ze1zsz.amplifyapp.com/',
-        image: two
+        image: five
     },
     {
-        id: 3,
+        id: 6,
         projectName: 'Handwriting Robot',
         projectDesc: 'This project was made for screen size 3840*2160 for a museum',
         tags: ['React', 'CSS'],
         code: '',
         demo: 'https://main.d28f4dbchyibef.amplifyapp.com/',
-        image: three
+        image: six
     },
     {
-        id: 4,
+        id: 7,
         projectName: 'Memory wall Projector',
         projectDesc: 'This project was made for screen size 3840*2160 for a museum',
         tags: ['React', 'CSS'],
         code: '',
         demo: 'https://main.duv7c7894ga9g.amplifyapp.com/',
-        image: four
+        image: seven
     },
     {
-        id: 5,
+        id: 8,
         projectName: 'Life Intro (Center Pillar)',
         projectDesc: 'This project was made for screen size 3840*2160 for a museum',
         tags: ['React', 'CSS'],
         code: '',
         demo: 'https://main.dq003bqlsdcb4.amplifyapp.com/',
-        image: five
+        image: eight
     },
     {
-        id: 6,
+        id: 9,
         projectName: 'Life Intro (LBS)',
         projectDesc: 'This project was made for screen size 3840*2160 for a museum',
         tags: ['React', 'CSS'],
         code: '',
         demo: 'https://main.dy4m64h03fgdk.amplifyapp.com/',
-        image: six
+        image: one
     },
     {
-        id: 7,
+        id: 10,
         projectName: 'Constituent assembly',
         projectDesc: 'This project was made for screen size 1080*1920 for a museum',
         tags: ['React', 'CSS'],
         code: '',
         demo: 'https://main.dyox7uw6xa3bz.amplifyapp.com/',
-        image: seven
+        image: two
     },
 ]
